e50893bb8793d97e6c2d5f41f5a4d0a5b6187f41
ibrahim <155033257+Boim274@users.noreply.github.com>
2024-11-13 00:00:21 +0700

    Tugas pak yudha Auth

--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,37 @@ Route::apiResource('/posts', App\Http\Controllers\Api\PostController::class);
 
 // fishing
 Route::apiResource('/fishings', App\Http\Controllers\Api\FishingController::class);
+
+/**
+ * route "/register"
+ * @method "POST"
+ */
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+
+/**
+ * route "/login"
+ * @method "POST"
+ */
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+
+/**
+ * route "/user"
+ * @method "GET"
+ */
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+/**
+ * route "/logout"
+ * @method "POST"
+ */
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+
+
+
+
+
+
+
+
